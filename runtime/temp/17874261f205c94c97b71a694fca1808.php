ce643168109f28813d48356a10ebb877ae1884a4
taixing <1509239073@qq.com>
2018-06-07 16:42:32 +0800

    new

---
+++ runtime/temp/17874261f205c94c97b71a694fca1808.php
@@ -0,0 +1,3 @@
+<?php /*a:1:{s:55:"C:\wamp\www\tp5\application\index\view\index\hello.html";i:1525339771;}*/ ?>
+<?php echo action('Blog/header', '', 'widget'); ?>
+<?php echo action('Blog/menu', ['name' => 'think'], 'widget'); ?>
